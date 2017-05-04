f03896dfac2ed438ef3f33df630f6d8001a2d279
Kieve Chua <kievechua@gmail.com>
2017-05-04 10:36:57 +0800

    rename config file to underscore

---
+++ dist/config/keen-tracking.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// https://keen.io/projects/<projectId>/access
+exports.default = {
+    projectId: '',
+    writeKey: ''
+};
+//# sourceMappingURL=keen-tracking.js.map
